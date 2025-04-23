f73b1a0d9f8565d41149d14dee6d3dae2906a8fe
krone <alvaro10048@protonmail.com>
2025-04-23 03:50:37 -0300

    feat: enhance build process with infer and cppcheck tasks

--- xmake.lua
+++ xmake.lua
@@ -17,3 +17,37 @@ task("clean")
         os.rm("build")
     end)
 
+
+task("infer")
+    set_menu { usage = "xmake infer", description = "Run Facebook Infer" }
+    on_run(function ()
+        os.exec("xmake project -k compile_commands")   -- cria ./compile_commands.json
+
+        local cpus = (os.cpuinfo and os.cpuinfo().ncpu) or 1
+        os.exec(string.format(
+            "infer run --compilation-database compile_commands.json "
+          .. "--headers --jobs %d", cpus))
+    end)
+
+
+
+task("cppcheck")
+    set_menu { usage = "xmake cppcheck", description = "Run Cppcheck" }
+
+    on_run(function ()
+        os.exec("xmake project -k compile_commands")  -- garante o DB
+
+        local flags = {
+            "--project=compile_commands.json",
+            "--std=c++23",
+            "--enable=all",
+            "--inconclusive",
+            "--suppress=missingIncludeSystem",  -- <-- aqui
+            "--quiet",
+            "--xml",
+            "--output-file=cppcheck-report.xml"
+        }
+
+        os.exec("cppcheck " .. table.concat(flags, " "))
+        print("✓ cppcheck concluído → cppcheck-report.xml")
+    end)
